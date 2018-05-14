e3f0c57a7993ce2298b1d72bb018c1ee45518c44
Guillaume Hivert <hivert.is.coming@gmail.com>
2018-05-14 19:01:57 +0200

    :recycle: Refactor footer

--- app/components/footer.rb
+++ app/components/footer.rb
@@ -5,30 +5,40 @@ module Components
     def render
       footer do
         div.container.flex.mobile_column do
-          div.flex_1.flex do
-            p style: "white-space: pre;" do
-              "Copyright © 2018 Guillaume Hivert. Theme heavily inspired by "
-            end
-            a href: "https://tabler.github.io/tabler",
-              target: "_blank",
-              onclick: -> (event) {} do
-              "Tabler"
-            end
-            p { "." }
-          end
-          div.mobile_top_padding.flex style: "align-items: center;" do
-            a href: "https://www.coindesk.com/price",
-              target: "_blank",
-              style: "padding-right: 12px;",
-              onclick: -> (event) {} do
-              "Powered by CoinDesk"
-            end
-            a.btn.btn_outline_primary.btn_sm href: "https://github.com/ghivert/RubyCoin",
-              target: "_blank",
-              onclick: -> (event) {} do
-              "Source code"
-            end
-          end
+          legal_mentions
+          source_and_obligations
+        end
+      end
+    end
+
+    private
+
+    def new_tab_link(params, &block)
+      params = { target: "_blank", onclick: -> (event) {} }.merge(params)
+      a(params, &block)
+    end
+
+    def legal_mentions
+      div.flex_1.flex do
+        p style: "white-space: pre;" do
+          "Copyright © 2018 Guillaume Hivert. Theme heavily inspired by "
+        end
+        new_tab_link href: "https://tabler.github.io/tabler" do
+          "Tabler"
+        end
+        p { "." }
+      end
+    end
+
+    def source_and_obligations
+      div.mobile_top_padding.flex style: "align-items: center;" do
+        new_tab_link href: "https://www.coindesk.com/price",
+                     style: "padding-right: 12px;" do
+          "Powered by CoinDesk"
+        end
+        new_tab_link href: "https://github.com/ghivert/RubyCoin",
+                     class: "btn btn_outline_primary btn_sm" do
+          "Source code"
         end
       end
     end
