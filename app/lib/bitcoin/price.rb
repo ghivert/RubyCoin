f6cdfc342245f1214c0fedf5fc5b8ebb27223dc0
Guillaume Hivert <hivert.is.coming@gmail.com>
2018-05-14 15:56:47 +0200

    :recycle: Improve Bitcoin price fetching function

--- app/lib/bitcoin/price.rb
+++ app/lib/bitcoin/price.rb
@@ -1,16 +1,19 @@
+require 'promise'
+require 'lib/net'
+
 ENDPOINT = 'https://api.coinmarketcap.com/v2'
 
 module Bitcoin
   class Price
     class << self
       def get_current
-        # ::HTTP.get(ENDPOINT + '/ticker/1?convert=EUR') do |response|
-        #   if response.ok?
-        #     puts response.body
-        #     puts response.json
-        #   else
-        #     puts "Wrong Request #{response}"
-        #   end
+        ::Net::HTTP.get(ENDPOINT + '/ticker/1/?convert=EUR')
+          .then { |request|
+            puts request
+            `console.log(#{request.to_n})`
+          }.fail { |request|
+            puts request
+          }
       end
     end
   end
