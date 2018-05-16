a9904742f1cb2eb323e19caa1a31a6e1ba6a6bda
Guillaume Hivert <hivert.is.coming@gmail.com>
2018-05-16 15:08:20 +0200

    :sparkles: Add Chart from Chart.js for bitcoin price

--- app/components/home.rb
+++ app/components/home.rb
@@ -1,4 +1,6 @@
-require 'lib/bitcoin'
+require "date"
+require "lib/bitcoin"
+require "lib/chart"
 
 module Components
   class Home
@@ -6,10 +8,43 @@ module Components
 
     def render
       div do
+        if store.bitcoin_values != {}
+          component chart
+        end
       end
+    end
+
+    private
+
+    def chart
+      @chart ||= initialize_chart
+    end
+
+    def initialize_chart
+      @chart = ::Chart.new height: "500px", options: {
+        type: "line",
+        data: {
+          labels: store.bitcoin_values.keys.map { |date| ::Date.parse date }.map { |date| date.strftime "%m/%d/%Y" },
+          datasets: [{
+            data: store.bitcoin_values.values,
+            backgroundColor: "rgb(254, 232, 185)",
+            borderColor: "rgb(252, 161, 21)",
+            borderWidth: 2,
+            pointRadius: 0
+          }]
+        },
+        options: {
+          legend: {
+            display: false
+          },
+          tooltips: {
+            mode: "nearest"
+          },
+          hover: {
+            intersect: false
+          }
+        }
+      } if @chart == nil
     end
   end
 end
-
-Bitcoin.get_current_price
-Bitcoin.get_past_month_price
