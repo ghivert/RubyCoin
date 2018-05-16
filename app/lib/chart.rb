b2e5812ca4c89eb299afc27b8b43b788ee3e241f
Guillaume Hivert <hivert.is.coming@gmail.com>
2018-05-16 15:07:11 +0200

    :sparkles: Add chart.js library

---
+++ app/lib/chart.rb
@@ -0,0 +1,29 @@
+require "securerandom"
+
+class Chart
+  include Inesita::Component
+
+  def initialize(options)
+    @width = options[:width]
+    @height = options[:height]
+    @options = options[:options]
+    @id = ::SecureRandom.uuid
+  end
+
+  def render
+    div.chart_container style: "position: relative; #{@height == nil ? "" : "height:#@height"}; #{@width == nil ? "" : "width:#@width"};" do
+      canvas id: @id, hook: hook(:render_chart)
+    end
+  end
+
+  private
+
+  def render_chart
+    node = `document.getElementById(#@id)`
+    if `#{node} == null`
+      `setTimeout(function() { #{self.render_chart} }, 1)`
+    else
+      @chart = `new Chart(#{node}.getContext('2d'), #{@options.to_n})`
+    end
+  end
+end
