e81509b308fb8216ed3c8df448bd4ec5cb9edec8
Guillaume Hivert <hivert.is.coming@gmail.com>
2018-05-16 18:11:38 +0200

    :recycle: Add fetching of bitcoin current price

--- app/store.rb
+++ app/store.rb
@@ -2,15 +2,24 @@ class Store
   include Inesita::Injection
 
   attr_accessor :bitcoin_values
+  attr_accessor :bitcoin_current_value
 
   def init
     @bitcoin_values = {}
+    @bitcoin_current_value = nil
     get_bitcoin_prices
   end
 
   private
 
   def get_bitcoin_prices
+    Bitcoin.get_current_price.then { |response|
+      @bitcoin_current_value = response[:response][:bpi][:EUR][:rate_float]
+      render!
+    }.fail { |response|
+      puts response
+    }
+
     Bitcoin.get_past_month_price.then { |response|
       @bitcoin_values = response[:response][:bpi]
       render!
