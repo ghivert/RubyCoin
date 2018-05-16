0101c60b7b69872256bcb758c72355d561529882
Guillaume Hivert <hivert.is.coming@gmail.com>
2018-05-16 15:28:48 +0200

    :recycle: Improve code separation in chart

--- app/components/home/chart.rb
+++ app/components/home/chart.rb
@@ -6,32 +6,42 @@ module Components
       def initialize(bitcoin_values)
         @chart = ::Chart.new height: "500px", options: {
           type: "line",
-          data: {
-            labels: bitcoin_values.keys.map { |date| ::Date.parse date }.map { |date| date.strftime "%m/%d/%Y" },
-            datasets: [{
-              data: bitcoin_values.values,
-              backgroundColor: "rgb(254, 232, 185)",
-              borderColor: "rgb(252, 161, 21)",
-              borderWidth: 2,
-              pointRadius: 0
-            }]
-          },
-          options: {
-            legend: {
-              display: false
-            },
-            tooltips: {
-              mode: "nearest"
-            },
-            hover: {
-              intersect: false
-            }
-          }
-        } if @chart == nil
+          data: generate_data(bitcoin_values),
+          options: generate_options
+        }
       end
 
       def render
         component @chart
+      end
+
+      private
+
+      def generate_data(bitcoin_values)
+        {
+          labels: bitcoin_values.keys.map { |date| ::Date.parse date }.map { |date| date.strftime "%m/%d/%Y" },
+          datasets: [{
+            data: bitcoin_values.values,
+            backgroundColor: "rgb(254, 232, 185)",
+            borderColor: "rgb(252, 161, 21)",
+            borderWidth: 2,
+            pointRadius: 0
+          }]
+        }
+      end
+
+      def generate_options
+        {
+          legend: {
+            display: false
+          },
+          tooltips: {
+            mode: "nearest"
+          },
+          hover: {
+            intersect: false
+          }
+        }
       end
     end
   end
