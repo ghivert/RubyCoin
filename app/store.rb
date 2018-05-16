f0089a9e23c7f469ab7fd859b2da530e7ac87c8a
Guillaume Hivert <hivert.is.coming@gmail.com>
2018-05-16 15:06:33 +0200

    :sparkles: Add bitcoin price fetching and store in store

--- app/store.rb
+++ app/store.rb
@@ -1,9 +1,21 @@
 class Store
   include Inesita::Injection
 
-  attr_accessor :bitcoin_value
+  attr_accessor :bitcoin_values
 
   def init
-    @bitcoin_value = {}
+    @bitcoin_values = {}
+    get_bitcoin_prices
+  end
+
+  private
+
+  def get_bitcoin_prices
+    Bitcoin.get_past_month_price.then { |response|
+      @bitcoin_values = response[:response][:bpi]
+      render!
+    }.fail { |response|
+      puts response
+    }
   end
 end
