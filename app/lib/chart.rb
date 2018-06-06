badb9eecd47f3c9d1aaa0abaf3ccaef7c85741b8
Guillaume Hivert <guillaume.hivert@outlook.com>
2018-06-06 14:45:33 +0200

    :bug: Remove chart if not removed by VirtualDOM

--- app/lib/chart.rb
+++ app/lib/chart.rb
@@ -13,15 +13,24 @@ class Chart
 
   def render
     div style: "position: relative; #{@height == nil ? "" : "height:#@height"}; #{@width == nil ? "" : "width:#@width"};" do
-      canvas height: @height, id: @id, hook: hook(:render_chart)
+      canvas height: @height, id: @id, hook: hook(:render_chart), unhook: unhook(:remove_chart)
     end
   end
 
   private
 
+  def remove_chart
+    node = `document.getElementById(#@id)`
+    if `#{node} !== null` && not(router.current_url?("/"))
+      parent = `#{node}.parentNode`
+      `#{node}.remove()`
+      `#{parent}.style = null`
+    end
+  end
+
   def render_chart
     node = `document.getElementById(#@id)`
-    if `#{node} == null`
+    if `#{node} === null`
       `setTimeout(function() { #{self.render_chart} }, 1)`
     else
       @chart = `new Chart(#{node}.getContext('2d'), #{@options.to_n})`
