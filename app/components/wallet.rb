7e244ea67c0522d24b1359dc0052b00239d45e01
Guillaume Hivert <guillaume.hivert@outlook.com>
2018-06-06 14:47:47 +0200

    :lipstick: Improve display on wallet

--- app/components/wallet.rb
+++ app/components/wallet.rb
@@ -1,14 +1,22 @@
+require './home/variation_cell'
+
 module Components
   class Wallet
     include Inesita::Component
 
     def render
       div.wallet do
-        div.card.card_wallet__balance do
-          text store.balance_of_rubycoin.to_s(10)
+        div.card do
+          div.card_header__variations.wallet_header do
+            component Components::Home::VariationCell.new("#{store.rubycoin_balance.to_s(10)} RBC", "On your account")
+            div style: "border-right: 1px solid rgba(0, 40, 100, 0.12);"
+            component Components::Home::VariationCell.new("#{store.ethereum_balance.to_s(10)} ETH", "On your account")
+          end
         end
         div.card.card_wallet__transactions do
-          text store.balance_of_rubycoin.to_s(2)
+          div do
+            text store.ethereum_balance.to_s(17)
+          end
         end
       end
     end
