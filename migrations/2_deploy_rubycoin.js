102fc49feb3f4f85e66e90f2de4f9e660c7cd55c
Guillaume Hivert <guillaume.hivert@outlook.com>
2018-06-06 14:46:12 +0200

    :recycle: Improve migrations

--- migrations/2_deploy_rubycoin.js
+++ migrations/2_deploy_rubycoin.js
@@ -5,15 +5,9 @@ var contract
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(RubyCoin, 1, 0, 0).then(function(instance) {
     contract = instance
-    return contract.totalSupply.call()
-  }).then(function(totalSupply) {
-    console.log(totalSupply)
     return contract.transfer(accounts[1], 100, {from: accounts[0]})
   }).then(function(transaction) {
-    console.log(transaction)
-    return contract.balanceOf.call(accounts[0])
-  }).then(function(balanceOfOwner) {
-    console.log(balanceOfOwner)
+    return
   }).catch(function(error) {
     console.log(error)
   })
