076684bab664792994d04e6290c6284718b86537
Guillaume Hivert <guillaume.hivert@outlook.com>
2018-06-14 11:47:24 +0200

    :bug: Make compiling work

--- contracts/RubyCoin.sol
+++ contracts/RubyCoin.sol
@@ -28,31 +28,36 @@ contract RubyCoin is Ownable {
   /// @dev Returns the total supply of existing token.
   function totalSupply() public pure returns (uint) {
     // Do Something here!
+    return 0;
   }
 
   /// @dev Returns the balance of tokens of someone.
   /// @param _owner The person you want to check balance.
   function balanceOf(address _owner) public view returns (uint) {
     // Do Something here!
+    return 0;
   }
 
-  /// @dev Transfer some token to someone else.
+  /// @dev Transfer some token to someone else. Should revert if not enough funds. Emit Transfer when done.
   /// @param _to The receiver of transfer.
   /// @param _value The amount to transfer.
   /// @return True if transfer is successfull, False otherwise.
   function transfer(address _to, uint _value) public returns (bool success) {
     // Do Something here!
+    return false;
   }
 
-  /// @dev Approve a third party to withdraw a certain amount of tokens for the owner.
+  /// @dev Approve a third party to withdraw a certain amount of tokens for the owner. Emit Approval when done.
   /// @param _spender The future approved spender.
   /// @param _value The maximum value the spender will be able to withdraw.
   /// @return True if approving successfull, False otherwise.
   function approve(address _spender, uint _value) public returns (bool success) {
     // Do Something here!
+    return false;
   }
 
   /// @dev Transfer some tokens form one account to the other if previously approved.
+  ///      Should revert if allowance or balance less than value. Emit Transfer when done.
   /// @param _from The account to send from.
   /// @param _to The account to send to.
   /// @param _value The amount to send.
@@ -63,6 +68,7 @@ contract RubyCoin is Ownable {
     uint _value
     // Do Something here!
   ) public returns (bool success) {
+    return false;
   }
 
   /// @dev Returns the amount a spender can withdraw for another account.
@@ -74,5 +80,6 @@ contract RubyCoin is Ownable {
     address _spender
   ) public view returns (uint remaining) {
     // Do something here!
+    return 0;
   }
 }
